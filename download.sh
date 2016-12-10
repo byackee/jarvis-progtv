e80e2bcd91df6fd91c8934a379b15b37dbb7aebc
byackee <vincent.fresnel@gmail.com>
2016-12-10 16:46:04 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-curl -k -o ./plugins/jarvis-progtv/temp.zip http://xmltv.dtdns.net/download/tnt_lite.zip
+curl -k -o ./plugins/jarvis-progtv/temp.zip http://xmltv.dtdns.net/download/tnt.zip
 unzip -o ./plugins/jarvis-progtv/temp.zip -d ./plugins/jarvis-progtv/
 rm ./plugins/jarvis-progtv/temp.zip
