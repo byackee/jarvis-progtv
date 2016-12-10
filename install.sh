dfd5770764e2839204fe0a59cd35ae43190c7377
byackee <vincent.fresnel@gmail.com>
2016-12-10 16:31:25 +0100

    changement de fournisseur

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ sudo apt-get install -y python3 python3-pip python3-lxml
 sudo pip3 install plac
 sudo pip3 install yattag
 sudo pip3 install pytz
-echo "@midnight                               wget "http://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" -o NUL -O ./plugins/jarvis-progtv/tvguide.xml --header "Referer: www.kazer.org" 2>NUL" >> ../../jarvis-events
-wget "http://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" -o NUL -O tvguide.xml --header "Referer: www.kazer.org" 2>NUL
-
+echo "@midnight                               ./plugins/jarvis-progtv/download.sh" >> ./jarvis-events
+-sh ./plugins/jarvis-progtv/download.sh		 +sh download.sh
