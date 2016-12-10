5c5f29d7221127f91435ee1f55e27f015dd108bf
byackee <vincent.fresnel@gmail.com>
2016-12-10 17:49:42 +0100

    change méthode install lxml

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
-sudo apt-get install -y python3 python3-pip python3-lxml
+sudo apt-get install -y python3 python3-pip
 sudo pip3 install plac
 sudo pip3 install yattag
 sudo pip3 install pytz
+sudo pip3 install lxml
 echo "@midnight                               ./plugins/jarvis-progtv/download.sh" >> ./jarvis-events
 curl -k -o ./temp.zip http://xmltv.dtdns.net/download/tnt.zip
 unzip -o ./temp.zip -d ./
