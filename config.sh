ab970e1898e5e006d3e331822a22393bcd6e856b
byackee <vincent.fresnel@gmail.com>
2016-12-10 16:41:37 +0100

    Update config.sh
    change name of channel

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@ jv_pg_pt_ip="https://www.kazer.org/tvguide.xml?u=" # ex: "192.168.1.1"
 jv_pg_pt_key=4cgh4dkv94d6w # default is 80
 
 pourquand='{ "devices":[
-{ "voiciladate": "FRA2.kazer.org", "nom": "deux"},
-{ "voiciladate": "TF11.kazer.org", "nom": "une"},
-{ "voiciladate": "FRA3.kazer.org", "nom": "trois"},
-{ "voiciladate": "FRA2.kazer.org", "nom": "quatre"},
-{ "voiciladate": "FRA5.kazer.org", "nom": "cinq"},
-{ "voiciladate": "M61.kazer.org", "nom": "six"}
+{ "voiciladate": "1", "nom": "une"},
+{ "voiciladate": "2", "nom": "deux"},
+{ "voiciladate": "3", "nom": "trois"},
+{ "voiciladate": "4", "nom": "quatre"},
+{ "voiciladate": "5", "nom": "cinq"},
+{ "voiciladate": "6", "nom": "six"}
 ]}'
