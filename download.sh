f6760ff33647582571b6109e3cc0ff0de127a7d1
byackee <vincent.fresnel@gmail.com>
2016-12-10 14:02:19 +0100

    correction bug lors du download

--- download.sh
+++
@@ -1,3 +0,0 @@
-curl -k -o ./plugins/jarvis-progtv/temp.zip https://www.kazer.org/tvguide.zip?u=4cgh4dkv94d6wC
-unzip -o ./plugins/jarvis-progtv/temp.zip -d ./plugins/jarvis-progtv/
-rm ./plugins/jarvis-progtv/temp.zip
--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ sudo apt-get install -y python3 python3-pip python3-lxml
 sudo pip3 install plac
 sudo pip3 install yattag
 sudo pip3 install pytz
-echo "@midnight                               ./plugins/jarvis-progtv/download.sh" >> ./jarvis-events
-curl -k -o ./temp.zip https://www.kazer.org/tvguide.zip?u=4cgh4dkv94d6wC
-unzip -o ./temp.zip -d ./
-rm ./temp.zip
+echo "@midnight                               wget "http://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" -o NUL -O ./plugins/jarvis-progtv/tvguide.xml 2>NUL" >> ./jarvis-events
+wget "http://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" -o NUL -O tvguide.xml 2>NUL
+
