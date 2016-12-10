5109884c9179af3f13d914b52e2484fbba7d96f4
byackee <vincent.fresnel@gmail.com>
2016-12-10 13:48:52 +0100

    add download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+curl -k -o ./plugins/jarvis-progtv/temp.zip https://www.kazer.org/tvguide.zip?u=4cgh4dkv94d6wC
+unzip -o ./plugins/jarvis-progtv/temp.zip -d ./plugins/jarvis-progtv/
+rm ./plugins/jarvis-progtv/temp.zip
