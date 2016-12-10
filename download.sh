5f6410a44ffda515b530144c00fcc2273f6c603f
byackee <vincent.fresnel@gmail.com>
2016-12-10 16:33:00 +0100

    changement fournisseur XML

---
+++ download.sh
@@ -0,0 +1,3 @@
+curl -k -o ./plugins/jarvis-progtv/temp.zip http://xmltv.dtdns.net/download/tnt_lite.zip
+unzip -o ./plugins/jarvis-progtv/temp.zip -d ./plugins/jarvis-progtv/
+rm ./plugins/jarvis-progtv/temp.zip
