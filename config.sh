39e3f9bfa21044f79d8c43df591a5f253e527d64
byackee <vincent.fresnel@gmail.com>
2016-12-10 14:40:46 +0100

    add channel

--- config.sh
+++ config.sh
@@ -5,11 +5,7 @@ pourquand='{ "devices":[
 { "voiciladate": "FRA2.kazer.org", "nom": "deux"},
 { "voiciladate": "TF11.kazer.org", "nom": "une"},
 { "voiciladate": "FRA3.kazer.org", "nom": "trois"},
-{ "voiciladate": "04/29/2006", "nom": "lanniversaire de Justine"},
-{ "voiciladate": "10/11/1981", "nom": "lanniversaire de Léa"},
-{ "voiciladate": "04/02/2006", "nom": "lanniversaire de Alicia"},
-{ "voiciladate": "11/22/2007", "nom": "lanniversaire de Lorenzo"},
-{ "voiciladate": "08/31/1974", "nom": "lanniversaire de Jibé"},
-{ "voiciladate": "11/30/2016", "nom": "le rendez-vous chez le Dentiste"},
-{ "voiciladate": "31/12", "nom": "Fin dannée"}
+{ "voiciladate": "FRA2.kazer.org", "nom": "quatre"},
+{ "voiciladate": "FRA5.kazer.org", "nom": "cinq"},
+{ "voiciladate": "M61.kazer.org", "nom": "six"}
 ]}'
