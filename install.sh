a5fc341f75ba0b32a0b664b8c5a1919ea9f91935
byackee <byackee@serveur>
2016-12-10 11:40:24 +0100

    modification du fichier install

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt-get install -y python3 python3-pip python3-lxml
 sudo pip3 install plac
 sudo pip3 install yattag
 sudo pip3 install pytz
-echo "@midnight                               curl -k -o ./plugins/jarvis-progtv/test.xml https://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" >> jarvis-events
+echo "@midnight                               curl -k -o ./plugins/jarvis-progtv/test.xml https://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC" >> ./jarvis-events
 curl -k -o ./plugins/jarvis-progtv/test.xml https://www.kazer.org/tvguide.xml?u=4cgh4dkv94d6wC
