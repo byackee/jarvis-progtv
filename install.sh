88e2378194766aa1da03fccffc23579dd8decfba
byackee <vincent.fresnel@gmail.com>
2016-12-11 13:52:36 +0100

    add install lxml with apt-get

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
-sudo apt-get install -y python3 python3-pip libxml2-dev libxslt1-dev
+sudo apt-get install -y python3 python3-pip libxml2-dev libxslt1-dev python3-lxml
 sudo pip3 install plac
 sudo pip3 install yattag
 sudo pip3 install pytz
