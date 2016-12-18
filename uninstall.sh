236b70070cf312160fd7982866c1d1490bf8f47b
byackee <vincent.fresnel@vfhome.ovh>
2016-12-18 13:05:22 +0100

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use only if you need to undo changes on the user system such as removing software
-sed -i".bak" '/tv/d' ../../jarvis-events
+sed -i".bak" '/tv/d' $jv_dir/jarvis-events
