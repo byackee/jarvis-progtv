c55d5621ba6e89a44af0b38c09790dbd12b7534f
byackee <vincent.fresnel@vfhome.ovh>
2016-12-18 10:36:09 +0100

    suppression de download.sh et modification du cron

--- download.sh
+++
@@ -1,3 +0,0 @@
-curl -k -o ./plugins/jarvis-progtv/temp.zip http://xmltv.dtdns.net/download/tnt.zip
-unzip -o ./plugins/jarvis-progtv/temp.zip -d ./plugins/jarvis-progtv/
-rm ./plugins/jarvis-progtv/temp.zip
