fd489d989051277242db1d686a8436efd3058c9b
byackee <vincent.fresnel@gmail.com>
2017-01-11 10:48:05 +0100

    changement de nom de variable

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 
-pourquand='{ "devices":[
-{ "voiciladate": "C1.telerama.fr", "nom": "une"},
-{ "voiciladate": "C2.telerama.fr", "nom": "deux"},
-{ "voiciladate": "C3.telerama.fr", "nom": "trois"},
-{ "voiciladate": "C4.telerama.fr", "nom": "quatre"},
-{ "voiciladate": "C5.telerama.fr", "nom": "cinq"},
-{ "voiciladate": "C6.telerama.fr", "nom": "six"}
+listechainetv='{ "devices":[
+{ "chainetv": "C1.telerama.fr", "nom": "une"},
+{ "chainetv": "C2.telerama.fr", "nom": "deux"},
+{ "chainetv": "C3.telerama.fr", "nom": "trois"},
+{ "chainetv": "C4.telerama.fr", "nom": "quatre"},
+{ "chainetv": "C5.telerama.fr", "nom": "cinq"},
+{ "chainetv": "C6.telerama.fr", "nom": "six"}
 ]}'
