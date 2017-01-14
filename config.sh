cd28595d2f96f6b4ba2ee6e2997e8e7bdfa3565a
byackee <vincent.fresnel@gmail.com>
2017-01-14 17:46:25 +0100

    ajout des numero de chaine en chiffres
    La reconnaissance vocal peut reconnaitre les chiffres en chiffres ou en lettres

--- config.sh
+++ config.sh
@@ -1,9 +1,15 @@
 
 listechainetv='{ "devices":[
 { "chainetv": "C1.telerama.fr", "nom": "une"},
+{ "chainetv": "C1.telerama.fr", "nom": "1"},
 { "chainetv": "C2.telerama.fr", "nom": "deux"},
+{ "chainetv": "C2.telerama.fr", "nom": "2"},
 { "chainetv": "C3.telerama.fr", "nom": "trois"},
+{ "chainetv": "C3.telerama.fr", "nom": "3"},
 { "chainetv": "C4.telerama.fr", "nom": "quatre"},
+{ "chainetv": "C4.telerama.fr", "nom": "4"},
 { "chainetv": "C5.telerama.fr", "nom": "cinq"},
-{ "chainetv": "C6.telerama.fr", "nom": "six"}
+{ "chainetv": "C5.telerama.fr", "nom": "5"},
+{ "chainetv": "C6.telerama.fr", "nom": "six"},
+{ "chainetv": "C6.telerama.fr", "nom": "6"}
 ]}'
